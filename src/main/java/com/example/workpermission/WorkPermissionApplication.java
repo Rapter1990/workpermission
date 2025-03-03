3e48451c41e08d3c5322739f27d5d1a1155b6eed
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-03-03 14:21:12 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/workpermission/WorkPermissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.workpermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPermissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workpermission/WorkPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workpermission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
